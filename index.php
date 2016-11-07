028b0529c6c287778c68ec8906c03f9ee9f722ca
DeadMoras <deadmoras3011@mail.ru>
2016-11-08 01:42:36 +0200

    csrf

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 
 require ROOT . '/framework/app.php';
+
+function csrf_token()
+{
+	$cookie = new \framework\other\Cookie;
+	return $cookie->get('csrf');
+}
